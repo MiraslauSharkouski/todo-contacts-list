fdd7e2a7b9c3604c38043b091a7924b2b1b0d76a
Miraslau <miraslausharkouski@gmail.com>
2025-06-02 09:22:36 +0300

    add validator utilite

---
+++ src/utils/validator.ts
@@ -0,0 +1,11 @@
+export class Validator {
+  static isUniqueGroupName(name: string): boolean {
+    const groups = StorageManager.getGroups();
+    return !groups.some((g) => g.name === name);
+  }
+
+  static isUniquePhoneNumber(phone: string): boolean {
+    const contacts = StorageManager.getContacts();
+    return !contacts.some((c) => c.phone === phone);
+  }
+}
