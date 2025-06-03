13e9c9012aa6f51ea9dc0f2e89ba79ec1b6a047d
Miraslau <miraslausharkouski@gmail.com>
2025-06-03 08:18:48 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/todo-contacts-list/", // ← важно!
+  base: "/todo-contacts-list/",
   plugins: [],
   build: {
     outDir: "dist",
